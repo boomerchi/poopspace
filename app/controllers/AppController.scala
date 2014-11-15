1b801d27c087edac37ee4eca7b3f7da0b40255d1
Jim Miller <miller.james01@gmail.com>
2014-11-15 02:57:00 -0800

    Complete minimum specification

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -2,30 +2,69 @@ package controllers
 
 import play.api.mvc._
 import play.api.mvc.BodyParsers.parse
-import play.api.libs.json.Json
+import play.api.libs.json._
 import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick._
 import play.api.Play.current
 
-object AppController extends Controller {
+import models.Poop
 
-  def index = TODO
+object AppController extends Controller {
+  
+  def create = Action { Poop.create; Ok("hi")}
+
+  def index() = Action { Redirect(controllers.routes.AppController.map) }
   
   def about = TODO
   
-  def add = TODO
+  def add = Action { 
+    Ok(views.html.add())
+  }
   
-  def feed = TODO
+  def feed = DBAction { implicit rs =>
+    val len = Poop.poops.list.length
+    Ok(len.toString)
+  }
   
-  def map = TODO
+  def map = Action {
+    Ok(views.html.map())
+  }
   
   def myPoops = TODO
   
   def poop(id: Long) = TODO
   
-  def apiAdd = TODO
+  def apiAdd() = DBAction { implicit rs =>
+    val body = rs.body.asFormUrlEncoded.getOrElse(Map())
+    val loc = body("location").headOption getOrElse ""
+    val lat = body("latitude").headOption getOrElse ""
+    val long = body("longitude").headOption getOrElse ""
+    val exp = body("experience").headOption getOrElse ""
+    val clean = body("cleanliness").headOption getOrElse ""
+    val fac = body("facilities").headOption getOrElse ""
+    val atmo = body("atmosphere").headOption getOrElse ""
+    val poop = body("poop").headOption getOrElse ""
+    val notes = body("notes").headOption getOrElse ""
+    
+    def getDouble(str: String): Double = java.lang.Double.parseDouble(str)
+    
+    def getInt(str: String): Int = Integer.parseInt(str)
+    
+    Poop.poops += (0, loc, getDouble(lat), getDouble(long),
+    	getInt(exp), getInt(clean), getInt(fac), getInt(atmo),
+    	getInt(poop), notes, Poop.now)
+    
+    Ok("success")
+  }
   
-  def apiNearbyPoops = TODO
+  def apiNearbyPoops = DBAction { implicit rs =>
+    val body = rs.body.asFormUrlEncoded.getOrElse(Map())
+    val lat = java.lang.Double.parseDouble(body("latitude").headOption getOrElse "")
+    val long = java.lang.Double.parseDouble(body("longitude").headOption getOrElse "")
+    val pooples = Poop.nearby(lat, long)
+    val jsArray = JsArray(pooples.toSeq map { Poop.poopleToJson(_) })
+    Ok(jsArray)
+  }
   
   def apiMyPoops = TODO
 }
--- app/models/Poop.scala
+++ app/models/Poop.scala
@@ -4,8 +4,11 @@ import java.sql.Timestamp
 
 import java.util.Random
 import scala.slick.driver.H2Driver.simple._
+import play.api.Play.current
+import play.api.libs.json._
 
-class Poop(tag: Tag) extends Table[(Long, String, Double, Double, Int, Int, Int, Int, Int, Timestamp)](tag, "POOP") {
+
+class Poop(tag: Tag) extends Table[(Long, String, Double, Double, Int, Int, Int, Int, Int, String, Timestamp)](tag, "POOP") {
   def id = column[Long]("POOP_ID", O.PrimaryKey, O.AutoInc)
   def locName = column[String]("LOC_NAME")
   def longitude = column[Double]("LONGITUDE")
@@ -16,19 +19,97 @@ class Poop(tag: Tag) extends Table[(Long, String, Double, Double, Int, Int, Int,
   def facilities = column[Int]("FACILITIES")
   def atmosphere = column[Int]("ATMOSPHERE")
   def poopRating = column[Int]("POOP_RATING")
+  def notes = column[String]("NOTES")
   def time = column[Timestamp]("TIME")
   
-  def * = (id, locName, longitude, latitude, experience,
-		   cleanliness, facilities, atmosphere, poopRating, time)
+  def * = (id, locName, longitude, latitude, experience, cleanliness,
+		  facilities, atmosphere, poopRating, notes, time)
   
   def available = time > Poop.availablePast
 }
 
 object Poop {
+  type PoopTuple = (Long, String, Double, Double, Int, Int, Int, Int, Int, String, Timestamp)
+  
   def poops = TableQuery[Poop]
+  
+  def now = new Timestamp(System.currentTimeMillis)
   
   def availablePast = {
     val fifteenMinutes: Long = 60 * 15
     new Timestamp(System.currentTimeMillis - fifteenMinutes)
+  }
+  
+  def create = {
+    val maybeUrl = current.configuration.getString("db.default.url")
+    val url = maybeUrl getOrElse {throw new Exception("Bad database config")}
+    val ref = Database.forURL(url)
+    
+    ref.withSession { implicit s => poops.ddl.create }
+  }
+  
+  def nearby(lat2: Double, long2: Double)(implicit session: Session): List[PoopTuple] = {
+    val poopList = poops.list
+    poopList filter { p => 
+      val lat1 = p._4
+      val long1 = p._3
+      nearbyDoubles(lat1, long1, lat2, long2)
+    }
+  }
+  
+  def nearbyDoubles(lat1: Double, long1: Double, 
+		  			lat2: Double, long2: Double): Boolean = {
+    val R = 6371
+    val (a1, a2, o1, o2) = (lat1.toRadians, lat2.toRadians, long1.toRadians, long2.toRadians)
+    println(a1 + "|" + o1 + "|" + a2 + "|" + o2 )
+    val aDiff = (a1 - a2).toRadians
+    val oDiff = (o1 - o2).toRadians
+    val a = Math.sin(aDiff/2) * Math.sin(aDiff/2) +
+    		Math.cos(a1) * Math.cos(a2) *
+    		Math.sin(oDiff/2) * Math.sin(oDiff/2)
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    val d = R * c
+    d < 0.15
+  }
+  
+  def timeAgo(poople: PoopTuple): String = {
+    val time = poople._11
+    val millisAgo = now.getTime() - time.getTime()
+    val secondsAgo: Int = (millisAgo / 1000).toInt
+    val minutesAgo = secondsAgo / 60;
+    val hoursAgo = minutesAgo / 60
+    val daysAgo = hoursAgo / 24
+    if(minutesAgo < 60) {
+      minutesAgo + " min(s) ago"
+    } else if(hoursAgo < 24) {
+      if(hoursAgo == 1) {
+        hoursAgo + " hour ago"
+      } else {
+        hoursAgo + " hours ago"
+      }
+    } else {
+      if(daysAgo == 1) {
+        daysAgo + " day ago"
+      } else {
+        daysAgo + " days ago"
+      }
+    }
+  }
+  
+  def poopleToJson(poople: PoopTuple): JsValue = {
+    val json: JsValue = JsObject(Seq(
+        "location" -> JsString(poople._2),
+        "longitude" -> JsNumber(poople._3),
+        "latitude" ->  JsNumber(poople._4),
+        "experience" -> JsNumber(poople._5),
+        "cleanliness" -> JsNumber(poople._6),
+        "facilities" -> JsNumber(poople._7),
+        "atmosphere" -> JsNumber(poople._8),
+        "poop" -> JsNumber(poople._9),
+        "notes" -> JsString(poople._10),
+        "time" -> JsString(timeAgo(poople))
+      )
+    )
+    json
   }
 }
